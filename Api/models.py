4183b486f1a5e2868f20d86ef65f6c62fb34af39
Tomek <tomel23@wp.pl>
2019-04-13 01:19:09 +0200

    update location list view

--- Api/models.py
+++ Api/models.py
@@ -1,6 +1,9 @@
 from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+
 
 class Location(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=200)
     localization=models.PointField(srid=4326,)
     elevation=models.FloatField(default=0.0)
--- Api/views.py
+++ Api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
 
 from Api.models import Location
 from Api.serializers import LocationSerializer
 
 
 class LocalizationListView(generics.ListAPIView):
-    queryset = Location.objects.all()
     serializer_class=LocationSerializer
+
+    def get_queryset(self):
+        user=self.request.user
+        return Location.objects.filter(user=user)
+
+
 
