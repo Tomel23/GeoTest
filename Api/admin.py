fe399e9049ba908d62517b19e93d4dfdb5c1b37e
Tomek <tomel23@wp.pl>
2019-04-12 23:02:39 +0200

    register model in admin site

--- Api/admin.py
+++ Api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Location
+
+
+@admin.register(Location)
+class LocationAdmin(OSMGeoAdmin):
+    list_display = ('name', 'localization', 'elevation')
 
 # Register your models here.
