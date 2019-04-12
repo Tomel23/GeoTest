91f18d0b77d1c9ee3bb9f60b28320205f4204cec
Tomek <tomel23@wp.pl>
2019-04-12 23:18:44 +0200

    Tests for Location model class

--- Api/tests.py
+++ Api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Location
+from django.contrib.gis.geos import Point
 
-# Create your tests here.
+class LocationTest(TestCase):
+
+    def setUp(self):
+        Location.objects.create(name='loc1',elevation=120.0,localization=Point(15.82031249779768,7.449624259166628))
+
+    def test_name_content(self):
+        location=Location.objects.get(id=1)
+        expected_object_name=f'{location.name}'
+        self.assertEqual(expected_object_name,'loc1')
