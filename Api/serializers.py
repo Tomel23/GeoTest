21084bb852a8393457269e508917bef4f7246695
Tomek <tomel23@wp.pl>
2019-04-15 02:23:38 +0200

    prepare urls for new views

--- Api/serializers.py
+++ Api/serializers.py
@@ -5,5 +5,6 @@ from Api.models import Location
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Location
-        fields=['name','localization','elevation']
+        model = Location
+        fields = ['name', 'localization', 'elevation']
+
--- Api/urls.py
+++ Api/urls.py
@@ -1,8 +1,16 @@
+from django.conf.urls import url
 from django.urls import path
 
 from Api import views
 
-app_name='Api'
-urlpatterns=[
-    path('',views.LocalizationListView.as_view(),name='location_list')
+app_name = 'Api'
+urlpatterns = [
+    path('', views.AllLocalizationListView.as_view(), name='location_list'),
+    url(r'^add/(?P<name>[^/]+)/(?P<lon>-?\d*(.\d+))/(?P<lat>-?\d*(.\d+))/$', views.LocationCreateView.as_view(),
+        name='location_create'),
+    url(r'^closest_location/(?P<lon>-?\d*(.\d+))/(?P<lat>-?\d*(.\d+))/$', views.ClosestLocationsView.as_view(),
+        name='closest_location_search'),
+    url(r'^fixed_location/(?P<lon>-?\d*(.\d+))/(?P<lat>-?\d*(.\d+))/(?P<dist>-?\d*(.\d+))/$', views.FixedDistanceLocationsView.as_view(),
+        name='fixed_location_search'),
+
 ]
